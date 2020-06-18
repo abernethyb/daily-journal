5c71b6b731b845015594bf5749581fab4bcef5cc
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-06-18 16:08:48 -0500

    refactored.  posts save to database every time now.  put a .then() at the end of post method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,20 +23,35 @@ API.getJournalData().then(
     () => {
         DOMPush.journalList(entries)
     }
-).then()
+)
 
+const getUserInput = () => {
+    let date = document.querySelector("#date").value
+    let concepts = document.querySelector("#concepts").value
+    let entry = document.querySelector("#entry").value
+    let mood = document.querySelector("#mood").value
+    let newEntry = recordEntry(date, concepts, entry, mood)
+    return newEntry
+    
+}
 
 //const container = document.querySelector(".entryForm")
+    
 
 document.querySelector(".saveEntry").addEventListener("click", event => {
     /*
         Collect the user put by selecting the input fields, one
         at a time, and accessing the `value` property
     */
-    let date = document.querySelector("#date").value
-    let concepts = document.querySelector("#concepts").value
-    let entry = document.querySelector("#entry").value
-    let mood = document.querySelector("#mood").value
-    API.saveJournalEntry(recordEntry(date, concepts, entry, mood))
-
+    let theUserInputFromUserinputFunction = getUserInput()
+    console.log(theUserInputFromUserinputFunction)
+    
+    API.saveJournalEntry(theUserInputFromUserinputFunction).then()
+    
 })
+//let newEntry = recordEntry(date, concepts, entry, mood)
+//API.saveJournalEntry(newEntry)
+
+
+
+//API.saveJournalEntry(newEntry)
